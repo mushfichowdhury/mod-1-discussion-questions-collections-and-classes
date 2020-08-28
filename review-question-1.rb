9c5b2eb2d8a4041641faf3a834147a074d10ed53
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-08-28 09:56:42 -0400

    DQ Done

--- review-question-1.rb
+++ review-question-1.rb
@@ -1,5 +1,5 @@
 ## QUESTION 1
-
+require 'pry'
 pokemon = [
   {
     "id": 1,
@@ -8,7 +8,7 @@ pokemon = [
     "height": 7,
     "is_default": true,
     "order": 1,
-    "weight": 69,
+    "weight": 60,
     "abilities": [
         {
             "is_hidden": true,
@@ -27,7 +27,7 @@ pokemon = [
   "height": 10,
   "is_default": true,
   "order": 1,
-  "weight": 90,
+  "weight": 69,
   "abilities": [
       {
           "is_hidden": true,
@@ -60,10 +60,34 @@ pokemon = [
 }
 ]
 
+#pokemon[0][:abilities][:ability][:url]
+
 
 # How would you get the url for Bulbasaur's ability?
+        #pokemon[0][:abilities][0][:ability][:url]
+
 # How would you return the first pokemon with base experience over 40?
+#    def pokemon_exp_40(pokemon)
+#        pokemon.find {|poke| poke[:base_experience] > 40}
+#    end
+
 # How would you return ALL OF THE pokemon with base experience over 40? (Gotta catch em all)
+#    def pokemon_exp_40(pokemon)
+#        pokemon.each {|poke| poke[:base_experience] > 40} #or find_all
+#    end
+
 # How would you return an array of all of the pokemon's names?
+#    def pokemon_name(pokemon)
+#        pokemon.map {|poke| poke[:name]}
+#    end
+
 # How would you determine whether or not the pokemon array contained any pokemon with a weight greater than 60?
 #  whatever method you use should return true if there are any such pokemon, false if not.
+
+#   def pokemon_weight(pokemon)
+#        pokemon.any?(pokemon[:weight] > 60)
+#    end 
+
+
+
+binding.pry
